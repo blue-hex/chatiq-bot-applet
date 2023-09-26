08ce047448f31797b6f8a33cbcb1d4087a144d62
poonam672 <poonam.batham@bluehexsoftware.com>
2023-09-26 17:26:47 +0530

    Added Chat Bot UI

--- js/core.js
+++ js/core.js
@@ -4,55 +4,95 @@
 export function initChat() {
     // Your HTML template
     const chatTemplate = `
-    <section class="msger">
-    <header class="msger-header">
-        <div class="msger-header-title">
-            <i class="fas fa-comment-alt"></i> SimpleChat
-        </div>
-        <div class="msger-header-options">
-            <span><i class="fas fa-cog"></i></span>
-        </div>
-    </header>
+    <div class="fixed bottom-4 right-4">
+    <!-- Open/Close Button -->
 
-    <main class="msger-chat">
-        <div class="msg left-msg">
-            <div class="msg-img" style="background-image: url(https://image.flaticon.com/icons/svg/327/327779.svg)">
-            </div>
 
-            <div class="msg-bubble">
-                <div class="msg-info">
-                    <div class="msg-info-name">BOT</div>
-                    <div class="msg-info-time">12:45</div>
+    <button id="toggle-chatbot-button"
+        class="bg-yellow-600 text-white p-2 rounded-full shadow-md hover:bg-yellow-600 focus:outline-none">
+        <img src="1540869642.svg" width="40px" height="30px">
+
+
+
+    </button>
+   
+    <!-- Chatbot Form -->
+    <div class="max-w-md mx-auto p-4">
+
+    <div id="chatbot-form" class="bg-yellow-600 hidden w-96 absolute bottom-16 right-4 p-4 rounded-lg shadow-lg">
+    <button onclick="history.back()">Go Back</button>
+
+    <strong class="text-white text-xl align-left cursor-pointer alert-del">&times;</strong>
+
+    
+    <div class="md:container md:mx-auto ">
+            <div class="max-w-sm bg-white border border-yellow-600 rounded-lg shadow dark:bg-white dark:border-yellow-600">
+                <a href="#">
+                    <div class="flex justify-center items-center bg-yellow-600">
+
+                        <img class="rounded-t-lg " src="milo-image.png" alt="" width="40px" height="30px" />
+                    </div>
+                </a>
+                <div class="p-5  bg-yellow-600">
+                    <a href="#">
+                        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">Awshad Ai
+                            Chatbox</h5>
+                    </a>
+                    <p class="text-center mb-3 font-normal text-white dark:text-gray-400">Seach are Knowledge base or start a
+                        chat. We are happy to help you </p>
                 </div>
+                <div class="p-5 text-center bg-white" id="start-conversation-button">
 
-                <div class="msg-text">
-                    Hi, welcome to SimpleChat! Go ahead and send me a message. 😄
-                </div>
+        
+                <div id="chat:FCxvu7NgcI" class="h-16 text-white pt-4 rounded-lg text-bl bg-yellow-600 tawk-card tawk-card-inverse tawk-card-small card--chat tawk-box-shadow-xsmall"><button type="button" class="tawk-button-hover tawk-custom-color tawk-custom-border-color tawk-button"><i class="tawk-icon tawk-icon-mobile-send"></i> New Conversation </button></div>                </div>
+
             </div>
         </div>
 
-        <div class="msg right-msg">
-            <div class="msg-img" style="background-image: url(https://image.flaticon.com/icons/svg/145/145867.svg)">
-            </div>
-
-            <div class="msg-bubble">
-                <div class="msg-info">
-                    <div class="msg-info-name">Sajad</div>
-                    <div class="msg-info-time">12:46</div>
-                </div>
-
-                <div class="msg-text">
-                    You can change your name in JS section!
-                </div>
-            </div>
+        <!-- Email Verification Form -->
+        <div id="email-verification" class="hidden mt-4 w-96 ">
+            <form id="email-form" class="space-y-4">
+              
+    
+                <label for="email" class="block font-semibold text-white">Verify Your Email:</label>
+                <input type="email" id="email" class="p-2 border border-gray-300 rounded-md"
+                    placeholder="Enter your email">
+                <button type="submit" class="bg-yellow-700	 text-white p-2 rounded-md">Start Chat</button>
+            </form>
         </div>
-    </main>
+        <div id="chat-conversation1" class="msg-bubble mb-4 hidden">
+        <!-- Chat messages go here -->
+    </div>
+  
 
-    <form class="msger-inputarea">
-        <input type="text" class="msger-input" placeholder="Enter your message...">
-        <button type="submit" class="msger-send-btn">Send</button>
-    </form>
-</section>
+        <!-- Chat Conversation Form -->
+        <div id="chat-conversation" class="hidden mt-4">
+            <form id="chat-form" class="space-y-4 ">
+
+
+                <div class="mt-4 flex">
+                    <input type="text" id="user-input" class=" flex-1 w-full rounded-lg p-2 border"
+                        placeholder="Type your message..." />
+                    <button type="submit" id="send-button"
+                        class="bg-yellow-600 hover:bg-yellow-600 text-white rounded-lg p-2 ml-2 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 5l7 7-7 7" />
+                        </svg>
+                    </button>
+
+
+            </form>
+        </div>
+    
+
+    </div>
+    
+    
+</div>
+
+
     `;
 
     // Create a div element to hold the chat UI
@@ -62,67 +102,89 @@ export function initChat() {
     // Attach the chat UI to the document body
     document.body.appendChild(chatDiv);
 
-    // Add event listeners and logic here
     // For example, handle form submission and display user messages
-    const form = document.querySelector('.msger-inputarea');
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const input = document.querySelector('.msger-input');
-        const message = input.value;
-        displayUserMessage(message);
-        input.value = '';
-        // Call a function to generate a response and display it
-        // generateBotResponse(message);
-    });
+    var alert_del = document.querySelectorAll('.alert-del');
+    alert_del.forEach((x) =>
+        x.addEventListener('click', function () {
+            x.parentElement.classList.add('hidden');
+        })
+    );
+
 }
 
-// Function to display a user message in the chat
-function displayUserMessage(message) {
-    const chat = document.querySelector('.msger-chat');
-    const userMsg = `
-    <div class="msg right-msg">
-    <div class="msg-img" style="background-image: url(https://image.flaticon.com/icons/svg/145/145867.svg)">
-    </div>
 
-    <div class="msg-bubble">
-        <div class="msg-info">
-            <div class="msg-info-name">Sajad</div>
-            <div class="msg-info-time">12:46</div>
-        </div>
-
-        <div class="msg-text">
-            You can change your name in JS section!
-        </div>
-    </div>
-</div>
-    `;
-    const userMsgDiv = document.createElement('div');
-    userMsgDiv.innerHTML = userMsg;
-    chat.appendChild(userMsgDiv);
-}
 
 // Function to generate and display a bot response
-function generateBotResponse(userMessage) {
-    const chat = document.querySelector('.msger-chat');
-    const botMsg = `
-    <div class="msg left-msg">
-    <div class="msg-img" style="background-image: url(https://image.flaticon.com/icons/svg/327/327779.svg)">
-    </div>
 
-    <div class="msg-bubble">
-        <div class="msg-info">
-            <div class="msg-info-name">BOT</div>
-            <div class="msg-info-time">12:45</div>
-        </div>
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleChatbotButton = document.getElementById("toggle-chatbot-button");
+    const chatbotForm = document.getElementById("chatbot-form");
+    const startConversationButton = document.getElementById("start-conversation-button");
+    const emailVerification = document.getElementById("email-verification");
+    const chatConversation = document.getElementById("chat-conversation");
+    const emailForm = document.getElementById("email-form");
+    const chatForm = document.getElementById("chat-form");
+    const sendButton = document.getElementById('send-button');
+    const chatConversation1 = document.getElementById('chat-conversation1');
+    const userInput = document.getElementById('user-input');
+    toggleChatbotButton.addEventListener("click", function () {
+        if (chatbotForm.classList.contains("hidden")) {
+            chatbotForm.classList.remove("hidden");
+        } else {
+            chatbotForm.classList.add("hidden");
+        }
+    });
 
-        <div class="msg-text">
-            Hi, welcome to SimpleChat! Go ahead and send me a message. 😄
-        </div>
-    </div>
-</div>
-    `;
-    const botMsgDiv = document.createElement('div');
-    botMsgDiv.innerHTML = botMsg;
-    chat.appendChild(botMsgDiv);
-}
+    startConversationButton.addEventListener("click", function () {
+        startConversationButton.style.display = "none";
+        emailVerification.style.display = "block";
+    });
+    startConversationButton.addEventListener("click", function () {
+        startConversationButton.style.display = "none";
+        emailVerification.style.display = "block";
+    });
+
+
+    emailForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        emailVerification.style.display = "none";
+        chatConversation.style.display = "block";
+    });
+
+    sendButton.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        // const input = document.querySelector('.msger-input');
+        const userMessage = userInput.value;
+
+        // Append user's message to the chat conversation
+        appendMessage('user', userMessage);
+
+        const botResponse = getBotResponse(userMessage);
+
+        // Append chatbot's response to the chat conversation
+        appendMessage('bot', botResponse);
+
+        // Clear the input field
+        userInput.value = '';
+
+        // Show the chat conversation container
+        chatConversation1.style.display = 'block';
+    });
+
+    function appendMessage(sender, message) {
+        const messageDiv = document.createElement('div');
+        messageDiv.className = `message ${sender}`;
+        messageDiv.textContent = message;
+        chatConversation1.appendChild(messageDiv);
+    }
+
+    function getBotResponse(userMessage) {
+      
+        return `Me: "${userMessage}"`;
+    }
+
+});
+
+
 
