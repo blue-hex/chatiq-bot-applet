eea3be92bda880004da54adac69e79159a05556b
Abhishek Choudhury <abhishek.choudhury@bluehexsoftware.com>
2024-03-28 20:27:34 +0530

    update and format

--- js/core.js
+++ js/core.js
@@ -1,98 +1,84 @@
-// simple-chat.js
 
-// Function to initialize the chat UI and event handlers
+
+
 export function initChat() {
-  // Your HTML template
+
   const chatTemplate = `
-    <div class="fixed bottom-4 right-4">
-    <!-- Open/Close Button -->
+  <div class="fixed bottom-4 right-4">
+  <button id="toggle-chatbot-button"
+      class="bg-blue-600 text-white rounded-full shadow-md hover:bg-blue-600 focus:outline-none">
+      <img src="https://iqsuite.io/assets/iq.png" class="w-12 h-12 rounded-full scale-125">
+  </button>
 
+  <!-- Chatbot Form -->
+  <div class="max-w-md mx-auto p-4">
+      <div id="chatbot-form"
+          class="bg-white hidden w-96 absolute bottom-16 right-4 p-4 rounded-3xl shadow-lg border border-neutral-200">
+          <div class="rounded-full align-left cursor-pointer alert-del">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                  class="w-6 h-6 text-black">
+                  <path fill-rule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
+                      clip-rule="evenodd" />
+              </svg>
+          </div>
+          <div class="md:container md:mx-auto ">
+              <div class="max-w-sm bg-white rounded-lg">
+                  <a href="#">
+                      <div class="flex justify-center items-center bg-white">
 
-    <button id="toggle-chatbot-button"
-        class="bg-yellow-600 text-white p-2 rounded-full shadow-md hover:bg-yellow-600 focus:outline-none">
-        <img src="1540869642.svg" width="40px" height="30px">
+                          <img class="h-16 w-16 " src="https://iqsuite.io/assets/iq.png" alt="" width="40px"
+                              height="30px" />
+                      </div>
+                  </a>
+                  <div class="p-5 bg-white">
+                      <a href="#">
+                          <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-black">Chat iQ</h5>
+                      </a>
+                      <p class="text-center mb-3 font-normal text-black">Transform any business and deploy a custom
+                          solution with our cutting edge AI. </p>
+                  </div>
+                  <div class="px-5 pb-5 text-center bg-white" id="start-conversation-button">
+                      <div id="chat:FCxvu7NgcI" class="text-white rounded-lg text-bl bg-blue-950 py-4 px-4 w-full">
+                          <button type="button"> Start Conversation </button>
+                      </div>
+                  </div>
 
+              </div>
+          </div>
+          <!-- Email Verification Form -->
+          <div id="email-verification" class="hidden mt-4 w-96 ">
+              <form id="email-form" class="space-y-4">
+                  <label for="email" class="block font-semibold text-white">Verify Your Email:</label>
+                  <input type="email" name="email" id="email"
+                      class="p-2 rounded-md bg-neutral-100 border border-neutral-200" placeholder="Enter your email">
+                  <button id="email-button" type="submit" class="bg-blue-950 text-white p-2 rounded-md">Start
+                      Chat</button>
+              </form>
+          </div>
+          <div id="chat-conversation1" class="msg-bubble chat-conversation1  mb-4 hidden">
 
+          </div>
+          <!-- Chat Conversation Form -->
+          <div id="chat-conversation" class="hidden mt-4">
+              <form id="chat-form" class=" space-y-4 ">
+                  <div class="mt-4 flex">
+                      <input type="text" id="user-input"
+                          class=" flex-1 w-full rounded-lg p-2 bg-neutral-100 border border-neutral-200"
+                          placeholder="Type your query" />
+                      <button type="submit" id="send-button"
+                          class="bg-blue-950 hover:bg-blue-900 text-white rounded-lg p-2 ml-2 inline-flex justify-center items-center">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                              class="w-6 h-6 text-white">
+                              <path
+                                  d="M3.478 2.404a.75.75 0 0 0-.926.941l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.404Z" />
+                          </svg>
+                      </button>
+              </form>
+          </div>
+      </div>
+  </div>`;
 
-    </button>
-   
-    <!-- Chatbot Form -->
-    <div class="max-w-md mx-auto p-4">
-
-    <div id="chatbot-form" class="bg-yellow-600 hidden w-96 absolute bottom-16 right-4 p-4 rounded-lg shadow-lg">
- 
-    <strong class="text-white text-xl align-left cursor-pointer alert-del">&times;</strong>
-
-    
-    <div class="md:container md:mx-auto ">
-            <div class="max-w-sm bg-white border border-yellow-600 rounded-lg shadow dark:bg-white dark:border-yellow-600">
-                <a href="#">
-                    <div class="flex justify-center items-center bg-yellow-600">
-
-                        <img class="rounded-t-lg " src="milo-image.png" alt="" width="40px" height="30px" />
-                    </div>
-                </a>
-                <div class="p-5  bg-yellow-600">
-                    <a href="#">
-                        <h5 class="text-center mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">Awshad Ai
-                            Chatbox</h5>
-                    </a>
-                    <p class="text-center mb-3 font-normal text-white dark:text-gray-400">Seach are Knowledge base or start a
-                        chat. We are happy to help you </p>
-                </div>
-                <div class="p-5 text-center bg-white" id="start-conversation-button">
-
-        
-                <div id="chat:FCxvu7NgcI" class="h-16 text-white pt-4 rounded-lg text-bl bg-yellow-600 tawk-card tawk-card-inverse tawk-card-small card--chat tawk-box-shadow-xsmall"><button type="button" class="tawk-button-hover tawk-custom-color tawk-custom-border-color tawk-button"><i class="tawk-icon tawk-icon-mobile-send"></i> New Conversation </button></div>                </div>
-
-            </div>
-        </div>
-
-        <!-- Email Verification Form -->
-        <div id="email-verification" class="hidden mt-4 w-96 ">
-            <form id="email-form" class="space-y-4">
-              
-    
-                <label for="email" class="block font-semibold text-white">Verify Your Email:</label>
-                <input type="email" name = "email" id="email" class="p-2 border border-gray-300 rounded-md"
-                    placeholder="Enter your email">
-                <button id="email-button" type="submit" class="bg-yellow-700	 text-white p-2 rounded-md">Start Chat</button>
-            </form>
-        </div>
-        <div id="chat-conversation1" class="msg-bubble chat-conversation1  mb-4 hidden">
-        <!-- Chat messages go here -->
-    </div>
-  
-
-        <!-- Chat Conversation Form -->
-        <div id="chat-conversation" class="hidden mt-4">
-            <form id="chat-form" class=" space-y-4 ">
-
-
-            <div class="mt-4 flex">
-                    <input type="text" id="user-input" class=" flex-1 w-full rounded-lg p-2 border"
-                        placeholder="Type your message..." />
-                    <button type="submit" id="send-button"
-                        class="bg-yellow-600 hover:bg-yellow-600 text-white rounded-lg p-2 ml-2 flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M9 5l7 7-7 7" />
-                        </svg>
-                    </button>
-
-
-            </form>
-        </div>
-    
-
-    </div>
-    
-    
-</div>
-
-
-    `;
 
   // Create a div element to hold the chat UI
   const chatDiv = document.createElement("div");
@@ -101,26 +87,21 @@ export function initChat() {
   // Attach the chat UI to the document body
   document.body.appendChild(chatDiv);
 
-  // For example, handle form submission and display user messages
   var alert_del = document.querySelectorAll(".alert-del");
   alert_del.forEach((x) =>
     x.addEventListener("click", function () {
       x.parentElement.classList.add("hidden");
     })
   );
-
 }
 
 
 
 // Function to generate and display a bot response
-
 document.addEventListener("DOMContentLoaded", function () {
   const toggleChatbotButton = document.getElementById("toggle-chatbot-button");
   const chatbotForm = document.getElementById("chatbot-form");
-  const startConversationButton = document.getElementById(
-    "start-conversation-button"
-  );
+  const startConversationButton = document.getElementById("start-conversation-button");
   const emailVerification = document.getElementById("email-verification");
   const chatConversation = document.getElementById("chat-conversation");
   const emailForm = document.getElementById("email-form");
@@ -128,6 +109,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const sendButton = document.getElementById("send-button");
   const chatConversation1 = document.getElementById("chat-conversation1");
   const userInput = document.getElementById("user-input");
+
+
   toggleChatbotButton.addEventListener("click", function () {
     if (chatbotForm.classList.contains("hidden")) {
       chatbotForm.classList.remove("hidden");
@@ -140,11 +123,11 @@ document.addEventListener("DOMContentLoaded", function () {
     startConversationButton.style.display = "none";
     emailVerification.style.display = "block";
   });
+
   startConversationButton.addEventListener("click", function () {
     startConversationButton.style.display = "none";
     emailVerification.style.display = "block";
   });
-  
 
   emailForm.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -156,15 +139,12 @@ document.addEventListener("DOMContentLoaded", function () {
     e.preventDefault();
 
     const userMessage = userInput.value;
-
     // Append user's message to the chat conversation
     appendMessage("user", userMessage);
-
     const botResponse = getBotResponse(userMessage);
 
     // Append chatbot's response to the chat conversation
     // appendMessage('bot', botResponse);
-
     // Clear the input field
     userInput.value = "";
 
@@ -203,7 +183,7 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function getBotResponse(userMessage) {
-    // Make a POST request to the Flask API
+    // API Call Here !!
     fetch("http://127.0.0.1:5000/milo_chat", {
       method: "POST",
       headers: {
@@ -216,7 +196,6 @@ document.addEventListener("DOMContentLoaded", function () {
     })
       .then((response) => response.json())
       .then((data) => {
-        // Handle the API response data
         const responseElement = document.getElementById("chat-conversation1");
         const chat = document.querySelector(".msg-bubble");
 
@@ -240,10 +219,10 @@ document.addEventListener("DOMContentLoaded", function () {
       })
       .catch((error) => console.error(error));
   }
+
+  // Email API
   document.getElementById("email-button").addEventListener("click", () => {
     const inputData = document.getElementById("email").value;
-
-    // Make a POST request to the Flask API
     fetch("http://127.0.0.1:5000/milo_chat", {
       method: "POST",
       headers: {
@@ -262,11 +241,9 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       })
       .then((data) => {
-        // Handle the API response data
         console.log(data);
       })
       .catch((error) => {
-        // Handle errors, including non-JSON responses
         console.error("Error:", error);
       });
   });
